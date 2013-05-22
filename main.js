fe67d5690f420b3e6cfd2db25e4e50782b71f79f
olls <olls@ovi.com>
2013-05-22 20:35:56 +0100

    Style and added source sites

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ var feeds = [
 	'http://feeds.nytimes.com/nyt/rss/World',
 	'http://feeds.reuters.com/Reuters/worldNews'
 ];
+var total = 20;
 
 var allFeeds = new Array();
-
 $(document).ready(function() {
 	var count = 0;
 	$.each(feeds, function () {
 		var feed = new google.feeds.Feed(this);
 		feed.setResultFormat('JSON_FORMAT');
-		feed.setNumEntries(4);
+		feed.setNumEntries(parseInt(total/feeds.length));
 		feed.load(function(result) {
 			if (!result.error) {
 				$.each(result.feed.entries, function() {
@@ -49,6 +49,7 @@ function putInHTML(feedsLoaded) {
 					'<li>' + entry.categories.join('</li><li>') + '</li>'
 				);
 			}
+			var source = getDomainName(entry.link);
 			var dispDate = new Date(entry.publishedDate).format("D, d M Y H:i");
 			$('#feed').append(
 				$('<li />').html(
@@ -57,6 +58,8 @@ function putInHTML(feedsLoaded) {
 							entry.title
 						).attr('href', entry.link).attr('target', '_blank')
 					).add(
+						$('<h4 />').html(source)
+					).add(
 						$('<p />').html(entry.contentSnippet)
 					).add(
 						$('<time />').html(dispDate)
@@ -64,5 +67,18 @@ function putInHTML(feedsLoaded) {
 				)
 			);
 		});
+	}
+}
+
+function getDomainName(url) {
+	domain_name_parts = url.match(/:\/\/(.[^/]+)/)[1].split('.');
+	if(domain_name_parts.length >= 3){
+		domain_name_parts[0] = '';
+	}
+	var domain = domain_name_parts.join('.');
+	if(domain.indexOf('.') == 0) {
+		return domain.substr(1);
+	} else {
+		return domain;
 	}
 }
