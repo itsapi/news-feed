9e4a9c689c594d753cc34eea8f39b74c51dda487
olls <olls@ovi.com>
2013-05-22 16:22:49 +0100

    Split adding to html from retreaving feeds

--- main.js
+++ main.js
@@ -1,17 +1,48 @@
 google.load("feeds", "1");
 
-function initialize() {
-	var feed = new google.feeds.Feed("http://fastpshb.appspot.com/feed/1/fastpshb");
-	feed.load(function(result) {
-		if (!result.error) {
-			for (var i = 0; i < result.feed.entries.length; i++) {
-				var entry = result.feed.entries[i];
-				$('#feed').append($('<li />').html(entry.title));
-				console.log(entry.title);
+var feeds = [
+	'http://feeds.bbci.co.uk/news/rss.xml',
+	'http://rss.cnn.com/rss/edition_world.rss',
+	'http://feeds.nytimes.com/nyt/rss/World',
+	'http://feeds.reuters.com/Reuters/worldNews'
+];
+
+$(document).ready(function() {
+	
+	var allFeeds = new Array(); // Defined here
+
+	var error = 0;
+	$.each(feeds, function () {
+		var feed = new google.feeds.Feed(this);
+		feed.setResultFormat('JSON_FORMAT');
+		feed.setNumEntries(1);
+		feed.load(function(result) {
+			if (!result.error) {
+				allFeeds.push(result.feed.entries); // Appended to here
+			} else {
+				error = 1;
 			}
-		} else {
-			alert('Error accessing feed!');
-		}
+		});
 	});
-}
-google.setOnLoadCallback(initialize);
+	console.log(allFeeds); // Nothing in it here!
+	if (!error) {
+		$.each(allFeeds, function() {
+			var entry = this;
+			$('#feed').append(
+				$('<li />').html(
+					$('<h3 />').html(
+						$('<a />').html(
+							entry.title
+						).attr('href', entry.link)
+					).add(
+						$('<p />').html(entry.contentSnippet)
+					).add(
+						$('<time />').html(entry.publishedDate)
+					)
+				)
+			);
+		});
+	} else {
+		alert('Error accessing feeds!');
+	}
+});
