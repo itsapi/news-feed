1788584d9967c461c6bf69515e25540410e74ff6
olls <faircliff.oliver@gmail.com>
2013-05-22 12:56:32 +0100

    Swapped out for jquery

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function initialize() {
 	var feed = new google.feeds.Feed("http://fastpshb.appspot.com/feed/1/fastpshb");
 	feed.load(function(result) {
 		if (!result.error) {
-			var container = document.getElementById("feed");
 			for (var i = 0; i < result.feed.entries.length; i++) {
 				var entry = result.feed.entries[i];
-				var div = document.createElement("div");
-				div.appendChild(document.createTextNode(entry.title));
-				container.appendChild(div);
+				$('#feed').append($('<li />').html(entry.title));
+				console.log(entry.title);
 			}
+		} else {
+			alert('Error accessing feed!');
 		}
 	});
 }
