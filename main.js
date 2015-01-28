3ac91eafc2d00d5901e5ddeeb0b8fd140f570c06
Oliver Faircliff <mail@oliverfaircliff.com>
2015-01-28 11:34:26 +0000

    Cleaned up fix for #1

--- main.js
+++ main.js
@@ -83,101 +83,104 @@ $(document).ready(function() {
 
 function displayFeeds(feeds, search) {
 	var allFeeds = []; // The feed contents
-	var count = 0;
-	function next() {
-		count++;
-		putInHTML(count, feeds, allFeeds);
-	}
-	$.each(feeds, function () {
+	var count = feeds.length;
 
+	$.each(feeds, function () {
 		// Get the feed URL
 		var query = 'site:' + this + ' ' + search;
 		google.feeds.findFeeds(query, function(result) {
+		  
 			if (result.error) {
 			  console.log('Error finding feed');
-			  return next();
+			  return count--;
 			}
+			
 			if (result.entries.length <= 0) {
 			  console.log('No results for feed');
-			  return next();
+        return count--;
 			}
+			
 			var feedURL = result.entries[0].url;
 			if (feedURL === '') {
 			  console.log('Feed not found');
-			  return next();
+        return count--;
 			}
+			
 			console.log('Fetching:', feedURL);
 			var feed = new google.feeds.Feed(feedURL);
 			feed.setResultFormat('JSON_FORMAT');
 			feed.setNumEntries(parseInt(total/feeds.length));
+			
 			feed.load(function(result) {
-			  console.log(result)
 				if (result.error) {
 				  console.log('Error fetching feed');
-				  return next();
+          return count--;
 				}
+			
 				$.each(result.feed.entries, function() {
 					allFeeds.push(this);
 				});
-				return next();
+			
+				count--;
+				if (count === 0) {
+          putInHTML(allFeeds);
+        }
 			});
 		});
 
 	});
 }
 
-function putInHTML(feedsLoaded, feeds, allFeeds) {
-	if (feedsLoaded == feeds.length) {
-		allFeeds.sort(function (a, b) {
-			var aDate = new Date(a.publishedDate);
-			var bDate = new Date(b.publishedDate);
-			if (aDate < bDate) {
-				return -1;
-			} else if (aDate > bDate) {
-				return 1;
-			} else {
-				return 0;
-			}
-		});
+function putInHTML(allFeeds) {
+	allFeeds.sort(function (a, b) {
+		var aDate = new Date(a.publishedDate);
+		var bDate = new Date(b.publishedDate);
+		if (aDate < bDate) {
+			return -1;
+		} else if (aDate > bDate) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
 
-		$('#feed').empty();
-		$.each(allFeeds.reverse(), function(n) {
-			var entry = this;
-			var tags;
-			if (entry.categories.length > 0) {
-				tags = $('<ul class="tags" />').html(
-					'<li>' + entry.categories.join('</li><li>') + '</li>'
-				);
-			}
-			var source = getDomainName(entry.link);
-			var dispDate = new Date(entry.publishedDate).format("D, d M Y H:i");
-			$('#feed').append(
-				$('<li />').html(
-					$('<h3 />').html(
-						$('<a />')
-						.html(entry.title)
-						.attr('href', entry.link)
-						.attr('target', '_blank')
-					).add(
-						$('<h4 />').html(
-							$('<a />')
-							.attr('href', 'http://' + source)
-							.attr('target', '_blank')
-							.html(source)
-						)
-					).add(
-						$('<p />').html(entry.contentSnippet)
-					).add(
-						$('<time />').html(dispDate)
-					).add(tags)
-				)
+	$('#feed').empty();
+	$.each(allFeeds.reverse(), function(n) {
+		var entry = this;
+		var tags;
+		if (entry.categories.length > 0) {
+			tags = $('<ul class="tags" />').html(
+				'<li>' + entry.categories.join('</li><li>') + '</li>'
 			);
-		});
-		$('#feed li').each(function() {
-			$(this).find('h3').css('margin-right', $(this).find('h4').css('width'));
-		});
-		console.log('Loaded Stories');
-	}
+		}
+		var source = getDomainName(entry.link);
+		var dispDate = new Date(entry.publishedDate).format("D, d M Y H:i");
+		$('#feed').append(
+			$('<li />').html(
+				$('<h3 />').html(
+					$('<a />')
+					.html(entry.title)
+					.attr('href', entry.link)
+					.attr('target', '_blank')
+				).add(
+					$('<h4 />').html(
+						$('<a />')
+						.attr('href', 'http://' + source)
+						.attr('target', '_blank')
+						.html(source)
+					)
+				).add(
+					$('<p />').html(entry.contentSnippet)
+				).add(
+					$('<time />').html(dispDate)
+				).add(tags)
+			)
+		);
+	});
+	$('#feed li').each(function() {
+		$(this).find('h3').css('margin-right', $(this).find('h4').css('width'));
+	});
+	console.log('Loaded Stories');
 }
 
 function getDomainName(data) {
